8abdf49e2146277489567e905c9702ad2415cf02
Matt Radford <matt.radford@sage.com>
2022-04-12 11:41:34 +0100

    Update wp-check-scheduled-posts-cli.php

--- wp-check-scheduled-posts-cli.php
+++ wp-check-scheduled-posts-cli.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Plugin Name: Check Scheduled Posts with WP-CLI
- * Description: Checks if any posts have missed their publishing time and publishes them.
- * Version: 1.0
- * Author: Robert Went
+ * Description: Checks if any posts are scheduled.
+ * Version: 1.0.0
+ * Author: Robert Went / Matt Radford
  * Author URI: https://www.robertwent.com
  * License: GPL3
  */
